96651157c5281d7645a9b1961d9f8d52892a0648
VictorPosada <115775393+VictorPosada@users.noreply.github.com>
2022-10-14 08:36:07 +0200

    Add files via upload

---
+++ foro.php
@@ -0,0 +1,35 @@
+<html>
+	<head>
+        <title>Foro</title>
+        <meta http-equiv="Content-Type"	content="text/html; charset=UTF-8" />
+        <meta name="author"	content="Victor Posada Soto, Raúl Mosquera Hoyos" />
+        <meta name="generator"	content="Visual Studio Code" />
+        <meta name="description"	content="Foro destinado al alumnado del Ies de Teis" />
+        <meta name="keywords"	content="Foro, Ies de teis, metas, javas y de todo" />
+        <!--css-->
+        <link rel="stylesheet" type="text/css" href="estilo.css" />
+	</head>
+	<body>			
+		<header>
+            <div class="container">
+                <div class="item_1"><?php print '<img class="logo" src="LogoTeis.jpg"/>'?></div>
+                <div class="item_2"><?='<p>Barra de busqueda</p>'?></div>
+                <div class="item_3"><?='<p>Registrate</p>'?></div>
+                <div class="item_4"><?='<p>Entra</p>'?></div>
+            </div>
+		</header>
+		<section>
+			<article>
+				<p>Enunciado exercicio.</p>
+				<p> 	
+                    <?php
+                        echo "Rdo. exercicio";	
+                    ?>	
+                </p>
+			</article>
+		</section>
+		<footer>
+			<p>IES de Teis</p>
+		</footer>
+	</body>
+</html>
